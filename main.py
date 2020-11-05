f55b78ea93b1793eb2bb4d919f19d68c5a726df6
Rishabh Ballal <rishabhballal@gmail.com>
2020-11-05 19:14:08 +0530

    added homepage route

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+import config
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = config.SECRET_KEY
+
+@app.route('/')
+def home():
+    return render_template('layout.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
