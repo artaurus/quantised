db961917925592d61e132bdce15a554724bfc7b4
Rishabh Ballal <rishabhballal@gmail.com>
2020-11-06 21:54:18 +0530

    corrected template name

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] = config.SECRET_KEY
 
 @app.route('/')
 def home():
-    return render_template('layout.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
