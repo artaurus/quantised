afdb3258e7ad27fdeeeb5bcef29c470a7db27def
artaurus <rishabhballal@gmail.com>
2020-11-12 12:36:48 +0530

    completed content delivery code

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
-function navBar(data) {
-  data.forEach((item, i) => {
-    const node = document.createElement('li')
-    const textnode = document.createTextNode(item.title)
-    node.appendChild(textnode)
-    document.getElementById('nav').appendChild(node);
+function nav(data) {
+  const ul = document.createElement('ul');
+  data.forEach(item => {
+    const li = document.createElement('li');
+    const text = document.createTextNode(item.title);
+    li.appendChild(text);
+    ul.appendChild(li);
+  });
+  const nav = document.querySelector('nav');
+  nav.appendChild(ul);
+}
+
+function content(title, data) {
+  para = document.querySelector('p');
+  data.forEach(item => {
+    if (item.title == title) {
+      para.innerHTML = item.content;
+    };
   });
 }
 
 function handler(data) {
-  navBar(data);
+  nav(data);
+  li = document.querySelectorAll('nav ul li');
+  li.forEach(item => {
+    item.addEventListener('click', () => content(item.innerHTML, data));
+  });
 }
 
-fetch('./chapters.json')
+fetch('chapters.json')
 .then(res => res.json())
 .then(data => handler(data))
 .catch(err => console.log(err));
