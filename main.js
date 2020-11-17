216f9ae3335fe3997e1f247fe9920e1c23cf9c9e
Rishabh Ballal <rishabhballal@gmail.com>
2020-11-17 12:13:17 +0530

    revamped as per modified data structure

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-function flipSequence() {
-  const main = document.querySelector('main');
-  const first = main.firstElementChild;
-  main.removeChild(first);
-  main.appendChild(first);
-}
+const homePage = () => location.href = 'https://artaurus.github.io/quantised';
 
-function renderState(state, data) {
-  const h3 = document.querySelector('h3');
-  const para = document.querySelector('p');
-  data.forEach(elm => {
-    if (elm.title == state.id) {
-      h3.innerHTML = elm.title;
-      para.innerHTML = elm.content;
-    }
-  });
+function renderState(route) {
+  const state = document.getElementById(route);
+  if (state == null) {
+    return homePage();
+  }
+
+  document.querySelector('h3').innerHTML = route;
+  fetch('chapters/' + route + '.txt')
+    .then(resp => resp.text())
+    .then(content => {
+      document.querySelector('p').innerHTML = content;
+    })
+    .catch(error => console.log(error));
+  document.title = 'Quantised - ' + route;
 
   let old = document.querySelector('.active');
   if (old != null) {
@@ -24,29 +24,48 @@ function renderState(state, data) {
   window.scrollTo(0, 0);
 }
 
-function handler(data) {
-  const ul = document.querySelector('ul');
-  data.forEach(elm => {
-    const li = document.createElement('li');
-    const text = document.createTextNode(elm.title);
-    li.appendChild(text);
-    ul.appendChild(li);
-  });
-
-  document.querySelector('h1').addEventListener('click', () => location.href = 'https://artaurus.github.io/quantised/');
-  li = document.querySelectorAll('nav ul li');
-  li.forEach(state => {
-    state.id = state.innerHTML;
-    state.addEventListener('click', () => renderState(state, data));
-  });
+function loadRoute() {
+  if (location.hash != '') {
+    const route = location.hash.slice(1).replace('%20', ' ');
+    renderState(route);
+    history.pushState({}, '', 'quantised/' + route);
+  }
 }
 
 if (window.innerWidth < 480) {
-  flipSequence();
+  const main = document.querySelector('main');
+  const first = main.firstElementChild;
+  main.removeChild(first);
+  main.appendChild(first);
 }
-window.addEventListener('orientationchange', () => flipSequence());
 
-fetch('chapters.json')
-  .then(res => res.json())
-  .then(data => handler(data))
-  .catch(err => console.log(err));
+const routes = ['Introduction', 'Formalism', 'Postulates', 'The atom', 'Symmetries', 'Entanglement'];
+
+document.querySelector('h1').addEventListener('click', () => homePage());
+
+const ul = document.querySelector('ul');
+routes.forEach(route => {
+  const li = document.createElement('li');
+  const text = document.createTextNode(route);
+  li.appendChild(text);
+  li.id = route;
+  ul.appendChild(li);
+
+  li.addEventListener('click', () => {
+    renderState(route);
+    history.pushState({}, '', 'quantised/' + route);
+  });
+});
+
+loadRoute();
+
+window.addEventListener('popstate', () => {
+  const route = location.pathname.split('/')[2].replace('%20', ' ');
+  if (route != '') {
+    renderState(route);
+  } else {
+    homePage();
+  }
+});
+
+window.addEventListener('orientationchange', () => location.reload());
