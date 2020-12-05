acbb0c6edd8b72795c9a1ab01a6aee5f9c4c0ab9
Rishabh Ballal <rishabhballal@gmail.com>
2020-12-05 19:39:39 +0530

    switched click events with anchors; updated routes

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const homePage = () => location.href = 'https://artaurus.github.io/quantised/';
 
 function renderState(route) {
-  const state = document.getElementById(route);
-  if (state == null) {
+  const state = document.querySelector('.' + route.replaceAll(' ', ''));
+  if (!state) {
     return homePage();
   }
 
@@ -15,11 +15,11 @@ function renderState(route) {
     .catch(error => console.log(error));
   document.title = 'Quantised - ' + route;
 
-  let old = document.querySelector('.active');
-  if (old != null) {
-    old.className = '';
+  let old = document.querySelector('#active');
+  if (old) {
+    old.id = '';
   }
-  state.className = 'active';
+  state.id = 'active';
   old = state;
   window.scrollTo(0, 0);
 }
@@ -31,32 +31,28 @@ if (window.innerWidth < 480) {
   main.appendChild(first);
 }
 
-const routes = ['Introduction', 'Wave-Particle Duality', 'Transformation Theory', 'Uncertainty', 'Spin', 'Antiparticles', 'Entanglement', 'Interpretations'];
-
-document.querySelector('h1').addEventListener('click', () => homePage());
+const routes = ['Introduction', 'Wave-Particle Duality', 'Transformation Theory', 'Uncertainty', 'Spin', 'Antiparticles', 'Entanglement', 'Interpretations', 'Measurement'];
 
 const ul = document.querySelector('ul');
 routes.forEach(route => {
   const li = document.createElement('li');
+  li.className = route.replaceAll(' ', '');
+  const a = document.createElement('a');
+  a.href = '#' + route.replaceAll(' ', '%20');
   const text = document.createTextNode(route);
-  li.appendChild(text);
-  li.id = route;
+  a.appendChild(text);
+  li.appendChild(a);
   ul.appendChild(li);
-
-  li.addEventListener('click', () => {
-    history.pushState({}, '', '#' + route);
-    renderState(route);
-  });
 });
 
-if (location.hash != '') {
-  const route = location.hash.slice(1).replace('%20', ' ');
+if (location.hash) {
+  const route = location.hash.slice(1).replaceAll('%20', ' ');
   renderState(route);
 }
 
 window.addEventListener('popstate', () => {
-  const route = location.hash.slice(1).replace('%20', ' ');
-  if (route != '') {
+  const route = location.hash.slice(1).replaceAll('%20', ' ');
+  if (route) {
     renderState(route);
   } else {
     homePage();
