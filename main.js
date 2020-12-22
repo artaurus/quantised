02deddf40ac4579b71b8b1fb05da0e860c19fd37
Rishabh Ballal <rishabhballal@gmail.com>
2020-12-22 12:11:28 +0530

    modified article headers

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 const homePage = () => location.href = 'https://artaurus.github.io/quantised/';
 
+function redesign(header) {
+  const displace = '20vh';
+  header.style.alignItems = 'center';
+  header.style.height = displace;
+  header.style.background = '#fff';
+  header.style.zIndex = '2';
+
+  if (window.innerWidth > 1120) {
+    document.querySelector('h1').style.fontSize = '3.5rem';
+  }
+  document.querySelector('main').style.top = displace;
+}
+
+function colorTile(index) {
+  let color = '';
+  switch (index % 3) {
+    case 0:
+      color = '#800080';
+      break;
+    case 1:
+      color = '#222';
+      break;
+    case 2:
+      color = '#008080';
+  }
+  return color;
+}
+
 function renderState(route) {
   window.scrollTo(0, 0);
 
-  const article = document.querySelector('article');
-  if (!article) {
+  const header = document.querySelector('header');
+  if (!document.querySelector('article')) {
     const sections = document.querySelectorAll('section');
     sections.forEach(section => section.style.display = 'none');
     const nav = document.querySelector('nav');
@@ -12,6 +40,8 @@ function renderState(route) {
 
     const article = document.createElement('article');
     document.querySelector('main').appendChild(article);
+
+    redesign(header);
   }
 
   fetch('chapters/' + route + '.txt')
@@ -27,6 +57,11 @@ function renderState(route) {
       document.title = 'Quantised - ' + route;
     })
     .catch(error => console.log(error));
+
+    const anchors = Array.from(document.querySelectorAll('h2 a'));
+    const routes = anchors.map(anchor => anchor.innerText);
+    const i = routes.findIndex(item => item == route.toUpperCase());
+    header.style.borderBottom = colorTile(i) + ' solid 4vh';
 }
 
 if (location.hash) {
@@ -41,18 +76,7 @@ if (location.hash) {
   });
 
   const tiles = Array.from(document.querySelectorAll('.nav-tile'));
-  tiles.forEach((tile, i) => {
-    switch (i % 3) {
-      case 0:
-        tile.style.backgroundColor = '#800080';
-        break;
-      case 1:
-        tile.style.backgroundColor = '#222';
-        break;
-      case 2:
-        tile.style.backgroundColor = '#008080';
-    }
-  });
+  tiles.forEach((tile, i) => tile.style.backgroundColor = colorTile(i));
 }
 
 window.addEventListener('hashchange', () => {
